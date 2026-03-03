15d19020beea01f18220e7c1d8090e6ee6a85037
Oliver Todreas <otodreas@gmail.com>
2026-03-03 11:55:48 +0100

    uncomment

--- src/5_remove_host_scaffolds.sh
+++ src/5_remove_host_scaffolds.sh
@@ -2,13 +2,13 @@
 
 cd "$(dirname "$0")"/..
 
-# # Retrieve the host scaffolds
-# ./src/datParser.py \
-#     data/4_blast/Ht.blastx \
-#     data/3_gene_predict_fasta/genemark_Ht.fna \
-#     server_data/taxonomy.dat \
-#     server_data/uniprot_sprot.dat >\
-#     data/5_no_avian_scaffolds/removed_contigs.txt
+# Retrieve the host scaffolds
+./src/datParser.py \
+    data/4_blast/Ht.blastx \
+    data/3_gene_predict_fasta/genemark_Ht.fna \
+    server_data/taxonomy.dat \
+    server_data/uniprot_sprot.dat >\
+    data/5_no_avian_scaffolds/removed_contigs.txt
 
 # Create new fna file without the removed contigs
 touch data/5_no_avian_scaffolds/genemark_Ht_no_avian.fna
